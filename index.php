ea458280b828c8710eb7d8cec6b201517f730cfa
Noah Cohn <noahc42@gmail.com>
2020-11-29 15:35:41 -0800

    Add files via upload
    Optimized code

--- index.php
+++ index.php
@@ -2,310 +2,311 @@
 <html lang="en">
 <head>
 <meta charset="utf-8">
-<meta http-equiv="cleartype" content="on">
-<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+<meta http-equiv="cleartype"content="on">
+<meta name="viewport"content="width=device-width,initial-scale=1.0">
 <title>B*3*H*D</title>
-<script src="./js/slideout.min.js"></script>
-<script type="text/javascript" src="./js/rSlider.min.js"></script>
+<script type="text/javascript"src="./js/slideout.min.js"></script>
+<script type="text/javascript"src="./js/rSlider.min.js"></script>
 <style>
-::-webkit-scrollbar {
- display: none;
+::-webkit-scrollbar{
+display:none;
 }
-#canvas {
- }
-#wrap {
- padding-top: 0;
- position: absolute;
- top: 50%;
- left: 50%;
- -moz-transform: translateX(-50%) translateY(-50%);
- -webkit-transform: translateX(-50%) translateY(-50%);
- transform: translateX(-50%) translateY(-50%);
+#canvas{
 }
-#wrapper {
+#wrap{
+padding-top:0;
+position:absolute;
+top:50%;
+left:50%;
+-moz-transform:translateX(-50%)translateY(-50%);
+-webkit-transform:translateX(-50%)translateY(-50%);
+transform:translateX(-50%)translateY(-50%);
 }
-body {
- background-color: black;
- overflow-x: hidden;
- overflow-y: scroll;
+#wrapper{
+}
+body{
+background-color:black;
+overflow-x:hidden;
+overflow-y:scroll;
 }
 #brand a,
-p a {
- border: 0px #fff;
- }
+p a{
+border:0px #fff;
+}
 #brand a:focus,
-p a:focus {
-outline: none;
- }
-p {
- padding-top: 0em;
+p a:focus{
+outline:none;
 }
-.px-video {
- top: 0;
- left: 0;
- margin-top: 0;
+p{
+padding-top:0em;
 }
-.px-video-container {
+.px-video{
+top:0;
+left:0;
+margin-top:0;
 }
-.px-video-wrapper {
+.px-video-container{
 }
-.slideout-menu {
- position: fixed;
- left: 0;
- top: 0;
- bottom: 0;
- right: 0;
- z-index: 0;
- width: 384px;
- overflow-y: scroll;
- -webkit-overflow-scrolling: touch;
- display: none;
- }
-.slideout-menu-left {
- left: 0;
+.px-video-wrapper{
 }
-.slideout-menu-right {
- right: 0;
+.slideout-menu{
+position:fixed;
+left:0;
+top:0;
+bottom:0;
+right:0;
+z-index:0;
+width:384px;
+overflow-y:scroll;
+-webkit-overflow-scrolling:touch;
+display:none;
 }
-.slideout-panel {
- z-index: 1;
- background-color: black;
- min-height: 100vh;
+.slideout-menu-left{
+left:0;
+}
+.slideout-menu-right{
+right:0;
+}
+.slideout-panel{
+z-index:1;
+background-color:black;
+min-height:100vh;
 }
 .slideout-open,
 .slideout-open body,
-.slideout-open .slideout-panel {
- overflow: hidden;
+.slideout-open .slideout-panel{
+overflow:hidden;
 }
-.slideout-open .slideout-menu {
- display: block;
+.slideout-open .slideout-menu{
+display:block;
 }
-</style><link rel="stylesheet" href="./css/rSlider.min.css"></head>
+</style><link rel="stylesheet"href="./css/rSlider.min.css"></head>
 <body>
 <nav id="menu">
-<section class="menu-section" id="menu-sections">
-<h2>Temporal Time:</h2>
+<section class="menu-section"id="menu-sections">
+B*3*H*D
+*******
 <ul class="menu-section-list">
-<div style="width: 384px;text color: white;">
-<div id="slideframe"><input type="text" id="timeslider" />
+<div style="width:384px;text color:white;">
+<div id="slideframe"><input type="text"id="timeslider"/>
 </div>
 </div>
 </ul>
 </section>
 </nav>
-<input type="checkbox" id="di" hidden="true" />
-<div id="iwid" hidden></div>
-<div id="ihig" hidden></div>
-<div id="wid" hidden></div>
-<div id="hig" hidden></div>
-<div id="inhred" hidden></div>
+<input type="checkbox"id="di"hidden="true"/>
+<div id="iwid"hidden></div>
+<div id="ihig"hidden></div>
+<div id="wid"hidden></div>
+<div id="hig"hidden></div>
+<div id="inhred"hidden></div>
 <var id="hiv">0</var>
-<div id="frate" hidden></div>
-<div id="temptime" hidden>4000</div>
-<div id="frptr" hidden></div>
+<div id="frate"hidden></div>
+<div id="temptime"hidden>7000</div>
+<div id="frptr"hidden></div>
 <main id="panel">
 <div id="wrapper">
-<div class="px-video-container" id="myvid">
-<div class="px-video-wrapper" id="wrap">
-<video hidden muted src="" name="playing" id="myvideo" height="" width="" preload="meta"></video>
-<video hidden muted src="" name="loading" id="loadvid" height="" width="" preload="auto"></video>
-<div id="canvas-parent" class="bh"></div>
-</div></div></div></main>
+<div class="px-video-container"id="myvid">
+<div class="px-video-wrapper"id="wrap">
+<div id="cp"class="bh"></div>
+</div></div></div></main><video hidden muted src=""name="playing"id="myvideo"height=""width=""preload="auto"></video>
+<video hidden muted src=""name="loading"id="loadv"height=""width=""preload="auto"></video>
 <script>
-var millisec, sidefram, slideout, timeslider, tem, dat, datb, pan, a, hms, higg, slitime, loopti, he, wi, adr, high, inhre, inhrez, ihe, rato, iwi, nrato, nvids, myvids, hig, men, di, looptime, rnum, seconds, rndtime, random, loopsecs, endc, loopti, lo, mv, vide;
-tem = document.getElementById("temptime");
-pan = document.getElementById("panel");
-sidefram = document.getElementById("slideframe");
-function grablooptime () {
-looptime = tem.innerHTML;
-looptime = (looptime * 10);
-looptime = Math.round(looptime);
-looptime = (looptime / 10);
+var mil,sfr,slo,tsl,tem,dat,datb,pan,a,hms,higg,slt,$loo,he,wi,adr,high,inhre,inhrez,ihe,rato,iwi,nrato,nvids,$vids,hig,men,di,$lt,rnum,$sc,$rtm,$rn,$ls,endc,lo,mv,vide;
+tem=document.getElementById("temptime");
+pan=document.getElementById("panel");
+sfr=document.getElementById("slideframe");
+function grab$lt(){
+$lt=tem.innerHTML;
+$lt=$lt*10;
+$lt=Math.round($lt);
+$lt=$lt/10;
 }
-grablooptime();
-slideout = new Slideout({
-"panel": document.getElementById("panel"),
-"menu": document.getElementById("menu"),
-"padding": 384,
-"tolerance": 70,
-"easing": "cubic-bezier(.32,2,.55,.27)"
+grab$lt();
+slo=new Slideout({
+"panel":document.getElementById("panel"),
+"menu":document.getElementById("menu"),
+"padding":384,
+"tolerance":70,
+"easing":"cubic-bezier(.32,2,.55,.27)"
 });
-pan.addEventListener("click", function() {
-slideout.toggle();
-sidefram.innerHTML = "";
-setTimeout( function () {
-sidefram.innerHTML = '<input type=' + '"te' + 'xt" id' + '="time' + 'slider" /' + '>';
-timeslider = new rSlider({
-target: "#timeslider",
-values: {min:1.0, max:18.0},
-step: [0.2],
-labels: false,
-tooltip: true,
-scale: false,
+pan.addEventListener("click",function(){
+slo.toggle();
+sfr.innerHTML="";
+setTimeout(function(){
+sfr.innerHTML='<input type='+'"te'+'xt"id'+'="time'+'slider"/'+'>';
+tsl=new rSlider({
+target:"#timeslider",
+values:{min:0.500,max:12.000},
+step:[0.100],
+labels:false,
+tooltip:true,
+scale:false,
 });
-grablooptime();
-slitime = (looptime / 1000);
-slitime = (slitime * 10);
-slitime = Math.round(slitime);
-slitime = (slitime / 10);
-timeslider.setValues(slitime);
-document.getElementById("menu").addEventListener("click", function() {
-loopti = timeslider.getValue();
-loopti = (loopti * 10);
-loopti = Math.round(loopti);
-loopti = (loopti / 10);
-loopti = (loopti * 1000);
-tem.innerHTML = loopti;
+grab$lt();
+slt=$lt/1000;
+slt=slt*10;
+slt=Math.round(slt);
+slt=slt/10;
+tsl.setValues(slt);
+document.getElementById("menu").addEventListener("click",function(){
+$loo=tsl.getValue();
+$loo=$loo*10;
+$loo=Math.round($loo);
+$loo=$loo/10;
+$loo=$loo*1000;
+tem.innerHTML=$loo;
 });
-setTimeout( function() {
-slitime = tem.innerHTML;
-}, 8);
-}, 16);
+setTimeout(function(){
+slt=tem.innerHTML;
+},8);
+},16);
 });
-nvids = <?php $cntr = file_get_contents("ctr.txt");echo "$cntr";?>;
-myvids = <?php $cnt = file_get_contents("vids.txt");echo "$cnt";?>;
-adr = myvids[0][0];
-wi = myvids[0][1];
-he = myvids[0][2];
-document.getElementById("hig").innerHTML = he;
-document.getElementById("wid").innerHTML = wi;
-inhre = window.innerHeight;
-inhre = Math.round(inhre);
-document.getElementById("inhred").innerHTML = inhre;
-iwi = window.innerWidth;
-rato = ((he / wi) * 10);
-rato = Math.round(rato);
-rato = (rato / 10);
-ihe = (iwi * rato);
-ihe = Math.round(ihe);
-dat = document.getElementById("inhred");
-datb = document.getElementById("ihig");
-inhrez = dat.innerHTML;
-high = (ihe - dat.innerHTML);
-if (high > 1) {
-ihe = dat.innerHTML;
-ihe = Math.round(ihe);
-nrato = (ihe / he);
-nrato = (nrato * 10);
-nrato = Math.round(nrato);
-nrato = (nrato / 10);
-iwi = (wi * nrato);
-iwi = Math.round(iwi);
+nvids=<?php $cntr=file_get_contents("ctr.txt");echo "$cntr";?>;
+$vids=<?php $cnt=file_get_contents("vids.txt");echo "$cnt";?>;
+adr=$vids[0][0];
+wi=$vids[0][1];
+he=$vids[0][2];
+document.getElementById("hig").innerHTML=he;
+document.getElementById("wid").innerHTML=wi;
+inhre=window.innerHeight;
+inhre=Math.round(inhre);
+document.getElementById("inhred").innerHTML=inhre;
+iwi=window.innerWidth;
+rato=(he/wi)*100;
+rato=Math.round(rato);
+rato=rato/100;
+ihe=iwi*rato;
+ihe=Math.round(ihe);
+dat=document.getElementById("inhred");
+datb=document.getElementById("ihig");
+inhrez=dat.innerHTML;
+high=ihe-dat.innerHTML;
+if(high>1){
+ihe=dat.innerHTML;
+ihe=Math.round(ihe);
+nrato=ihe/he;
+nrato=nrato*100;
+nrato=Math.round(nrato);
+nrato=nrato/100;
+iwi=wi*nrato;
+iwi=Math.round(iwi);
 };
-higg = (ihe + "px");
-document.getElementById("ihig").innerHTML = ihe;
-document.getElementById("iwid").innerHTML = iwi; 
-document.getElementById("wrap").style.lineheight = higg;
-document.getElementById("wrap").style.height = higg;
-document.getElementById("myvideo").src = adr;
-document.getElementById("myvideo").width = iwi;
-document.getElementById("myvideo").height = ihe;
+higg=ihe+"px";
+document.getElementById("ihig").innerHTML=ihe;
+document.getElementById("iwid").innerHTML=iwi;
+document.getElementById("wrap").style.lineheight=higg;
+document.getElementById("wrap").style.height=higg;
+document.getElementById("myvideo").src=adr;
 document.getElementById("myvideo").play();
-function loada() {
-inhre = window.innerHeight;
-inhre = Math.round(inhre);
-document.getElementById("inhred").innerHTML = inhre;
-looptime = tem.innerHTML;
-loadtime = (looptime * .9);
-loadtime = Math.round(loadtime);
-loopsecs = (looptime / 1000) + ( 2 * (loadtime / 1000));
-loadtime = Math.round(loadtime);
-random = Math.random();
-random = (random * 10000000);
-rnum = (random * nvids);
-rnum = (rnum / 10000000);
-rnum = Math.round(rnum);
-fp = myvids[rnum][6];
-fp = (fp * 1000);
-fp = Math.round(fp);
-fp = (fp / 1000);
-if (fp = 600) {
-fp = 60;
+function loada(){
+inhre=window.innerHeight;
+inhre=Math.round(inhre);
+document.getElementById("inhred").innerHTML=inhre;
+$lt=tem.innerHTML;
+$ldt=$lt*.4;
+$ldt=Math.round($ldt);
+$ls=($lt/1000)+(2*($ldt/1000));
+$ldt=Math.round($ldt);
+$rn=Math.random();
+$rn=$rn*10000000;
+rnum=$rn*nvids;
+rnum=rnum/10000000;
+rnum=Math.round(rnum);
+fp=$vids[rnum][6];
+fp=fp*1000;
+fp=Math.round(fp);
+fp=fp/1000;
+if(fp=600){
 }
-fp = (fp * 3);
-fp = (1000 / fp);
-fp = (fp * 1000);
-fp = Math.round(fp);
-fp = (fp / 1000);
-document.getElementById("frate").innerHTML = fp;
-wi = myvids[rnum][1];
-he = myvids[rnum][2];
-document.getElementById("hig").innerHTML = he;
-document.getElementById("wid").innerHTML = wi;
-adr = myvids[rnum][0];
-iwi = window.innerWidth;
-hms = myvids[rnum][3];
-a = hms.split(":");
-seconds = ((+a[0]) * 60) + (+a[1]);
-rndtime = (Math.random());
-millisec = (seconds * 1000);
-rndtime = (rndtime * 1000000);
-rndtime = (rndtime * millisec);
-rndtime = (rndtime / 1000000);
-pltime = Math.round(rndtime);
-pltime = (pltime / 1000);
-pltime = (pltime * 1000);
-pltime = Math.round(pltime);
-pltime = (pltime / 1000);
-pltime = (pltime - loopsecs);
-function endavoid() {
-pltime = (pltime - .5);
-endc = (seconds - pltime);
-if (endc < loopsecs) {
+// fp=fp*6;
+fp=1000/fp;
+fp=fp*1000;
+fp=Math.round(fp);
+fp=fp/1000;
+document.getElementById("frate").innerHTML=fp;
+wi=$vids[rnum][1];
+he=$vids[rnum][2];
+document.getElementById("hig").innerHTML=he;
+document.getElementById("wid").innerHTML=wi;
+adr=$vids[rnum][0];
+iwi=window.innerWidth;
+hms=$vids[rnum][3];
+a=hms.split(":");
+$sc=((+a[0])*60)+(+a[1]);
+$rtm=Math.random();
+mil=$sc*1000;
+$rtm=$rtm*10000000;
+$rtm=$rtm*mil;
+$rtm=$rtm/10000000;
+$plt=Math.round($rtm);
+$plt=$plt/1000;
+$plt=$plt*1000;
+$plt=Math.round($plt);
+$plt=$plt/1000;
+$plt=$plt-$ls;
+function endavoid(){
+$plt=$plt-1;
+endc=$sc-$plt;
+if(endc<$ls){
 endavoid();
-} else {};
+}else{};
 };
-function ifend() {
-endc = (seconds - pltime);
-if (endc < loopsecs) {
+function ifend(){
+endc=$sc-$plt;
+if(endc<$ls){
 endavoid();
 }
 };
 ifend();
-rato = ((he / wi) * 10);
-rato = Math.round(rato);
-rato = (rato / 10);
-ihe = (iwi * rato);
-ihe = Math.round(ihe);
-dat = document.getElementById("inhred");
-inhrez = dat.innerHTML;
-high = (ihe - dat.innerHTML);
-if (high > 1) {
-dat = document.getElementById("inhred");
-datb = document.getElementById("ihig");
-ihe = dat.innerHTML;
-datb.innerHTML = ihe;
-nrato = (ihe / he);
-nrato = (nrato * 10);
-nrato = Math.round(nrato);
-nrato = (nrato / 10);
-iwi = (wi * nrato);
-iwi = Math.round(iwi);
+rato=(he/wi)*100;
+rato=Math.round(rato);
+rato=rato/100;
+ihe=iwi*rato;
+ihe=Math.round(ihe);
+dat=document.getElementById("inhred");
+inhrez=dat.innerHTML;
+high=ihe-dat.innerHTML;
+if(high>1){
+dat=document.getElementById("inhred");
+datb=document.getElementById("ihig");
+ihe=dat.innerHTML;
+datb.innerHTML=ihe;
+nrato=ihe/he;
+nrato=nrato*100;
+nrato=Math.round(nrato);
+nrato=nrato/100;
+iwi=wi*nrato;
+iwi=Math.round(iwi);
 };
-higg = (ihe + "px");
-document.getElementById("ihig").innerHTML = ihe;
-document.getElementById("iwid").innerHTML = iwi;
-document.getElementById("loadvid").src = adr;
-document.getElementById("loadvid").width = iwi;
-document.getElementById("loadvid").height = ihe;
-document.getElementById("loadvid").currentTime = pltime;
-document.getElementById("loadvid").play();
-setTimeout(function () {
-}, loadtime);
-setTimeout(function () {
-vide = document.querySelectorAll("video");
-mv = vide[0].id;lo = vide[1].id;
-vide[0].id = lo;vide[1].id = mv;
-document.getElementById("wrap").style.lineheight = higg;
-document.getElementById("wrap").style.height = higg;
+higg=ihe+"px";
+setTimeout(function(){
+},$ldt);
+vide=document.querySelectorAll("video");
+document.getElementById("ihig").innerHTML=ihe;
+document.getElementById("iwid").innerHTML=iwi;
+document.getElementById("loadv").width=iwi;
+document.getElementById("loadv").height=ihe;
+document.getElementById("loadv").src=adr;
+document.getElementById("loadv").currentTime=$plt;
+document.getElementById("loadv").play();
+setTimeout(function(){
+document.getElementById("wrap").style.lineheight=higg;
+document.getElementById("wrap").style.height=higg;
+mv=vide[0].id;
+lo=vide[1].id;
+vide[0].id=lo;
+vide[1].id=mv;
 document.getElementById("di").click();
-}, loadtime);
-setTimeout(function () {
+document.getElementById("loadv").pause();
+
+},$ldt);
+setTimeout(function(){
 loada();
-}, looptime);
+},$lt);
 }
 loada();
 </script>
-<script src="./js/gpu-web.js"></script>
-<script async src="./js/b3hd.js"></script>
+<script type="text/javascript"src="./js/gpu-web.js"></script>
+<script type="text/javascript"async src="./js/b3hd.js"></script>
 </body></html>
